6b0af2a7e57511f6454947f4d2ec945ef35fc893
Yang Gao <pigirons@hotmail.com>
2022-02-01 00:19:41 +0800

    sgemm_kernel to sgemm_kernel_m1.S

--- build_sgemm_kernel.sh
+++ build_sgemm_kernel.sh
@@ -1,4 +1,3 @@
-as -o sgemm_kernel_aarch64.o sgemm_kernel_k4_aarch64.S
-#as -o sgemm_kernel_aarch64.o sgemm_kernel_k1_aarch64.S
+as -o sgemm_kernel_m1.o sgemm_kernel_m1.S
 clang -O3 -pthread -c test_sgemm_kernel.c
-clang -O3 -pthread -o test_sk test_sgemm_kernel.o sgemm_kernel_aarch64.o
+clang -O3 -pthread -o test_sk test_sgemm_kernel.o sgemm_kernel_m1.o
